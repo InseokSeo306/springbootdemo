35cf4ef5acb97fde40aa5db98116adf8db4a1ca5
InseokSeo <inseokseo0524@gmail.com>
2023-10-09 17:00:34 +0900

    내용 추가.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("My First Java Program.");
+		System.out.println("내용추가");
 	}
 
 }
